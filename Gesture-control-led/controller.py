e4f8e851a9b956d2c2b9f6b16412c514f5930e5c
MD Inzamamul Lohani <hamimlohani@gmail.com>
2025-07-02 20:53:38 +0600

    add comment

--- Gesture-control-led/controller.py
+++ Gesture-control-led/controller.py
@@ -2,20 +2,21 @@ import pyfirmata
 # pyfirmata for arduino pin
 # pyserial for serial monitor
 
-comport = '/dev/cu.usbmodem101'
+comport = '/dev/cu.usbmodem101' # Change this to your Arduino's port
 
-board = pyfirmata.Arduino(comport)
+board = pyfirmata.Arduino(comport) # Initialize the Arduino board
 
-led_pins = [9, 10, 11, 12, 13]
-leds = [board.get_pin(f'd:{pin}:o') for pin in led_pins]
+led_pins = [9, 10, 11, 12, 13] # Define the LED pins
+leds = [board.get_pin(f'd:{pin}:o') for pin in led_pins] # Create a list of LED pins as output pins
 
-def reset_leds():
+def reset_leds(): 
     for led in leds:
         led.write(0)
 
 
 def led(fingerUp):
+    # Control the LEDs based on the finger status
     if not isinstance(fingerUp, list) or len(fingerUp) != 5:
         raise ValueError("fingerUp must be a list of 5 elements (0 or 1).")
-    for i in range(5):
+    for i in range(5): # Iterate through each finger
         leds[i].write(1 if fingerUp[i] else 0)
